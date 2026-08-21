93beec718cb9294ed4c2eaadad3939631a19238d
Nikola Blanarova <nikola.blanarova@thermofisher.com>
2026-08-21 17:36:53 +0200

    fix: response time calculation

--- tests/post.spec.ts
+++ tests/post.spec.ts
@@ -1,14 +1,12 @@
 import { test, expect } from '@playwright/test';;
 import users from '../test-data/users-post.json';
 
-const RESPONSE_TIME_THRESHOLD = 1000; // in milliseconds
+const RESPONSE_TIME_THRESHOLD = 150; // in milliseconds
 
 for (const user of users) {
     test(`POST user to api - ${user.name}`, async ({ request }) => {
-        const beforeTime = Date.now();
         const response = await request.post('/api/users', { data: user });
-        const responseTime = Date.now() - beforeTime;                
-
+        
         const responseData = await response.json();
         await test.step('Validate response status, and data', async () => {
             expect(response.status()).toBe(201);
@@ -20,8 +18,10 @@ for (const user of users) {
                 job: user.job
             });
         });
-
+        
         await test.step('Validate response time', async () => {
+            const responseTime = response.timing().responseEnd - response.timing().requestStart;
+            console.log(`Response time for user ${user.name}: ${responseTime} ms`);
             expect(responseTime).toBeLessThan(RESPONSE_TIME_THRESHOLD);
         });
     });
