2c9e38a68c6df594bb49f142bbe4adf434b0b64f
Nikola Blanarova <nikola.blanarova@thermofisher.com>
2026-08-21 12:30:13 +0200

    test: create test for task 1 - GET users

---
+++ tests/get.spec.ts
@@ -0,0 +1,33 @@
+import { test, expect } from '@playwright/test';
+import expectedData from '../test-data/users-get-response.json';
+
+test('GET all users', async ({ request }) => {
+    const response = await request.get('/api/users?per_page=20');
+    expect(response.status()).toBe(200);
+    const responseData = await response.json();
+    await test.step('Compare response data', async () => {
+        expect(responseData.total).toBe(expectedData.total);
+        expect(responseData.data[0].last_name).toBe(expectedData.data[0].last_name);
+        expect(responseData.data[1].last_name).toBe(expectedData.data[1].last_name);
+        expect(responseData.data.length).toBe(responseData.total);
+    });
+
+    // Bonus task
+    await test.step('Validate data types', async () => {
+        expect(typeof responseData.page).toBe('number');
+        expect(typeof responseData.page).toBe(expectedData.page);
+        expect(typeof responseData.per_page).toBe('number');
+        expect(typeof responseData.per_page).toBe(expectedData.per_page);
+        expect(typeof responseData.total).toBe('number');
+        expect(typeof responseData.total).toBe(expectedData.total);
+        expect(typeof responseData.total_pages).toBe('number');
+        expect(typeof responseData.total_pages).toBe(expectedData.total_pages);
+        expect(typeof responseData.data).toBe('object');
+        expect(typeof responseData.data).toBe(expectedData.data);
+        expect(typeof responseData.data[0].id).toBe('number');
+        expect(typeof responseData.data[0].email).toBe('string');
+        expect(typeof responseData.data[0].first_name).toBe('string');
+        expect(typeof responseData.data[0].last_name).toBe('string');
+        expect(typeof responseData.data[0].avatar).toBe('string');
+    });
+});
